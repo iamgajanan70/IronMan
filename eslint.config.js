af5b26d176e188f15265df080e6b979196eb5d8d
Gajanan Waichale <someonespecial522@gmail.com>
2026-07-18 17:54:30 +0530

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -4,6 +4,7 @@ import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
+
 export default defineConfig([
   globalIgnores(['dist']),
   {
@@ -13,6 +14,7 @@ export default defineConfig([
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
     ],
+    
     languageOptions: {
       globals: globals.browser,
       parserOptions: { ecmaFeatures: { jsx: true } },
